89a5fc76347329b4464f3bb2bafaf775db5222c0
Linh Pham <linhpn2402@gmail.com>
2021-07-28 18:13:03 +0700

    clickable circle

--- agm-project/src/app/app.component.ts
+++ agm-project/src/app/app.component.ts
@@ -45,7 +45,7 @@ export class AppComponent implements OnInit {
 
   onChoseLocation(event: any) {
     console.log(event, 'test');
-    this.showInfoWindow = !this.showInfoWindow;
+    // this.showInfoWindow = !this.showInfoWindow;
   }
 
   
@@ -169,10 +169,11 @@ export class AppComponent implements OnInit {
       });
     });
 
-    this.http.get(this.url).subscribe((data) => {
+    // this.http.get(this.url).subscribe((data) => {
+    this.http.get(this.url).toPromise().then((data) => {
       // Object data
       console.log(data);
-      this.dataTable = data; // lấy dữ liệu hiện ra màn hình, dữ liệu chưa xử lý switch case
+      // this.dataTable = data; // lấy dữ liệu hiện ra màn hình, dữ liệu chưa xử lý switch case
 
       // marker: đã xử lý tất cả trường thông tin
       let temp: Object[] | any;
