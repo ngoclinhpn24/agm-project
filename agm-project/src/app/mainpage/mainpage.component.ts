1599329e0dd5897c5d6e9f2b82424555d1fb6ba5
Linh Pham <linhpn2402@gmail.com>
2021-08-23 17:40:33 +0700

    location within radius

--- agm-project/src/app/mainpage/mainpage.component.ts
+++ agm-project/src/app/mainpage/mainpage.component.ts
@@ -100,7 +100,7 @@ export class MainpageComponent implements OnInit {
           console.log("Radius: ", _radius);
           this.radius = _radius;
         })
-        
+      
         // body: Điểm tìm kiếm
         var body = {
           address: this.addressSearch,
@@ -123,14 +123,14 @@ export class MainpageComponent implements OnInit {
 
         // Tính khoảng cách
         // Dùng Filter để lọc, xóa những địa điểm không phù hợp luôn 
-        // let temp3 = this.markerLatLng;
-        // this.markerInsideRadius = temp3.filter((loc: any) => {
-        //   var markerLoc = new google.maps.LatLng(loc.lat, loc.lng);
-        //   const distanceInKm = google.maps.geometry.spherical.computeDistanceBetween(markerLoc, searchLoc)/1000;
-        //   if(distanceInKm < 10.0){
-        //     return loc;
-        //   }
-        // });
+        let temp3 = this.marker;
+        this.markerInsideRadius = temp3.filter((loc: any) => {
+          var markerLoc = new google.maps.LatLng(loc.lat, loc.lng);
+          const distanceInKm = google.maps.geometry.spherical.computeDistanceBetween(markerLoc, searchLoc)/1000;
+          if(distanceInKm < 10.0){
+            return loc;
+          }
+        });
         console.log('Result Inside Radius: ', this.markerInsideRadius);
 
          // HTTP GET, {fromObject: {lat: this.searchResult.lat , lng: this.searchResult.lng}}
@@ -141,8 +141,9 @@ export class MainpageComponent implements OnInit {
             radius: body.radius
           }
         });
+
         // lấy data file json
-        // this.http.get(this.url, {params: params} ).toPromise().then((data) => 
+        
         this.http.get(this.url, {params: httpParams}).subscribe((data) => {
             // marker: đã xử lý tất cả trường thông tin
             let temp: Object[] | any;
@@ -240,7 +241,100 @@ export class MainpageComponent implements OnInit {
 
       });
 
+      this.http.get(this.url).subscribe((data) => {
+        // marker: đã xử lý tất cả trường thông tin
+        let temp: Object[] | any;
+        temp = data;
+        this.marker = temp.map((location: any) => {
+          var concaveType = location.concaveType;
+          var concaveCode = location.concaveCode;
+          var source = location.source;
+          var address = location.address;
+          var lat: number = +location.lat;
+          var lng: number = +location.lng;
+          var radius: number = +location.radius;
+          var areaName = location.areaName;
+          var provinceName = location.provinceName;
+          var districtName = location.districtName;
+          var villageName = location.villageName;
+          var ruralName = location.ruralName;
+          var locationName = location.locationName;
+          switch (concaveType) {
+            case 0:
+              concaveType = '2G';
+              break;
+            case 1:
+              concaveType = '3G';
+              break;
+            case 2:
+              concaveType = '4G';
+              break;
+          }
+  
+          switch (source) {
+            case 0:
+              source = 'Bản ghi MRR';
+              break;
+            case 1:
+              source = 'Đo kiểm PAKH';
+              break;
+            case 2:
+              source = 'Đo kiểm driving test';
+              break;
+            case 3:
+              source = 'Mô phỏng ATOLL';
+              break;
+            case 4:
+              source = 'Cung cấp từ VTT ';
+              break;
+          }
+          return {
+            concaveCode: concaveCode,
+            concaveType: concaveType,
+            source: source,
+            address: address,
+            lat: lat,
+            lng: lng,
+            radius: radius,
+            areaName: areaName,
+            provinceName: provinceName,
+            districtName: districtName,
+            villageName: villageName,
+            ruralName: ruralName,
+            locationName: locationName,
+          };
 
+        });
+
+        // convert lat, lng, radius sang number => circle
+        let temp1: Object[] | any;
+        temp1 = data;
+        
+        this.circle = temp1.map((location: any) => {
+          var lat: number = +location.lat; 
+          var lng: number = +location.lng;
+          var radius: number = +location.radius;
+  
+          return {
+            lat: lat,
+            lng: lng,
+            radius: radius,
+          };
+        }); 
+        
+         // Lay lat, lng => tính khoảng cách
+        let temp2: Object[] | any;
+        temp2 = data;
+    
+        this.markerLatLng= temp2.map((location: any) => {
+          var lat: number = +location.lat; 
+          var lng: number = +location.lng;
+          return {
+            lat: lat,
+            lng: lng,
+          };
+        }); 
+    });
     });
   
   }
